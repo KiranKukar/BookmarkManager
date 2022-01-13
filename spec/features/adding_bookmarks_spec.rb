c924d4431139624f6786196cfe87be7b5f409b2b
KiranKukar <kiranrkukar@gmail.com>
2022-01-13 18:00:59 +0000

    wrapped database data in rogram objects

--- spec/features/adding_bookmarks_spec.rb
+++ spec/features/adding_bookmarks_spec.rb
@@ -3,8 +3,9 @@ feature "adds a bookmark" do
     visit('/bookmarks')
 
     fill_in 'url', with: 'www.website.co.uk'
+    fill_in 'title', with: 'Website title'
     click_on 'Create'
 
-    expect(page).to have_content 'www.website.co.uk'
+    expect(page).to have_link('Website title', href: 'www.website.co.uk')
   end
 end
